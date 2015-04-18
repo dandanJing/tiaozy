cb61ad4668c7deb1401d8d4874c5a8f259ea8fd8
jing-owner <1544299536@qq.com>
2015-04-18 10:06:40 +0800

    修改上传文件部分

--- application/html_files/static/scripts/pub_1.js
+++ application/html_files/static/scripts/pub_1.js
@@ -9,3 +9,24 @@ $(document).ready(function(){
     });
 });
 
+function checkImgType(ths){  
+    if (ths.value == "") {  
+         toastr.error("请上传图片");
+        return false;  
+    } else {  
+        if (!/\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/.test(ths.value)) {  
+            toastr.error('图片类型有误,图片类型必须是.gif,jpeg,jpg,png中的一种');      
+            return false;  
+        }  
+    }  
+    return true;  
+}  
+
+function selectfile(file){
+    if(checkImgType($(this))){
+        alert(1);
+         // var p = $("#upload-file").value;
+         // $(".upload-inner b").css({"background":p});
+     }else{
+     } 
+}
