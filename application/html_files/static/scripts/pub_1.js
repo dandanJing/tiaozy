30b7adb8090c62ed77d4bdc8178d5501767ba106
jing-owner <1544299536@qq.com>
2015-04-29 14:51:39 +0800

    修改发布部分

--- application/html_files/static/scripts/pub_1.js
+++ application/html_files/static/scripts/pub_1.js
@@ -10,29 +10,91 @@ $(document).ready(function(){
     });
     $(".upload-inner").hover(function(){
     });
+    $(".upload-inner").children().removeClass("active");
+    var active_index = -1;
+    for(var i=1;i<$(".upload-inner").children().length;i++){
+        var inputObj = $(".upload-inner").children().eq(i);
+        if(inputObj.val()!=""){
+            var imgobj = inputObj.get(0).files[0];
+            var strsrc = getObjectURL(imgobj);
+            if($("#images-ul").children().length < 5){
+                appendImageChild(strsrc,i-1);
+            }
+            inputObj.css({"width":"1px","height":"1px","display":"inline"});
+        }else if(active_index==-1){
+            active_index = i;
+        }
+    }
+    console.log("active_index:"+active_index);
+    if(active_index>-1){
+        setAsActiveInput(active_index);
+    }
+    
 });
 
-function selectfile(ths,change_index){
+function setAsActiveInput(active_index){
+    if(active_index< $(".upload-inner").children().length){
+        var obj = $(".upload-inner").children().eq(active_index);
+        obj.toggleClass("active");
+        obj.css({"width":"90px","height":"90px","display":"inline"});
+    }
+}
 
+function selectfile(ths,change_index){
+    console.log(change_index);
     if(checkImgType(ths)){
         var imgobj = $(ths).get(0).files[0];
         var strsrc = getObjectURL(imgobj);
-        if(change_index < $("#images-ul").children().length){
-            var liObj = $("#images-ul").children().eq(change_index);
-            var imgObject = document.createElement('img');
-            imgObject.src = strsrc;
-            $(imgObject).val(change_index);
-            liObj.children().eq(0).remove();
-            $(imgObject).toggleClass("display-image");
-            liObj.prepend(imgObject);
-            $(liObj).css({"display":"inline"});
-        }
-        
-        if(change_index+2 <6){
+        if($("#images-ul").children().length < 5){
+            appendImageChild(strsrc,change_index);
             $(ths).css({"width":"1px","height":"1px"});
-            $(ths).parent().children().eq(change_index+2).toggleClass("active");
+            $(ths).removeClass("active");
+            for(var i = 1;i<$(ths).parent().children().length;i++){
+                var obj = $(ths).parent().children().eq(i);
+                console.log("input "+i+"val: "+obj.val());
+                if(obj.val()==""){
+                    obj.toggleClass("active");
+                    obj.css({"width":"90px","height":"90px","display":"inline"});
+                    break;
+                }
+            }
+        }else{
+            toastr.error('最多上传5张图片');
+            return;
         }
      }
+}
+
+function appendImageChild(strsrc,change_index){
+    var innerHtml = "<li>";
+    innerHtml += "<img src=\""+strsrc+"\"  class=\"display-image\" data-value=\""+change_index+"\">";
+    innerHtml += "<img src=\"/static/images/delete.jpg\" class=\"delete-image\" onclick=\"deleteFile(this)\"></li>";
+    var html_strat = $("#images-ul").html()+innerHtml;
+    $("#images-ul").html(html_strat);
+}
+
+function  deleteFile(ths){
+    if($(ths).parent().children().length >1){
+        var input_index = $(ths).parent().children().eq(0).data('value');
+        console.log("delete input : "+input_index);
+        if($(".upload-inner").children().length > input_index+1){
+            console.log($(".upload-inner").children().eq(input_index+1).val());
+            $(".upload-inner").children().eq(input_index+1).val('');
+            var i;
+            for(i = 0; i< $(".upload-inner").children().length;i++){
+                if($(".upload-inner").children().eq(i).hasClass("active")){
+                    break;
+                }
+            }
+            console.log('find active index:' + i);
+            if(i==$(".upload-inner").children().length){
+                $(".upload-inner").children().eq(input_index+1).toggleClass("active");
+                $(".upload-inner").children().eq(input_index+1).css({"width":"90px","height":"90px","display":"inline"});
+            }
+            $(ths).parent().remove();
+        }  
+    }
+    
 }
 
 function submit_form(){
